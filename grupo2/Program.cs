3d77a8e246e9b688a86bf293e1e046ce2e961c15
User <User@DESKTOP-2MA536R>
2024-09-27 14:34:47 -0400

    Agregar archivos de proyecto.

---
+++ grupo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grupo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("buenas nobches a todos, somos el grupo2");
+        }
+    }
+}
